a1c45bf7c2216dc668c93aba1d06d9960bcc4c93
Brandon Patterson <brandnpatterson@gmail.com>
2019-06-17 22:56:16 -0500

    set up enzyme and create first integration test

---
+++ client/src/__mockData__/index.js
@@ -0,0 +1,18 @@
+export const users = [
+  {
+    firstname: "Michael",
+    lastname: "Scott",
+    username: "mjscott",
+    email: "mjscott@gmail.com",
+    location: "Scranton, TX",
+    jobtitle: "Regional Manager"
+  },
+  {
+    firstname: "Jim",
+    lastname: "Halpert",
+    username: "jhalpert",
+    email: "jhalpert@gmail.com",
+    location: "Scranton, PA",
+    jobtitle: "Paper Salesman"
+  }
+];
---
+++ client/src/__tests__/App.test.js
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+import { Context } from "../context";
+import App from "../App";
+import Users from "../pages/Users";
+import { MemoryRouter, Route } from "react-router";
+import { shallow } from "enzyme";
+import mockAxios from "axios";
+import { users } from "../__mockData__";
+import { fetchUsers } from "../context/api";
+
+describe("<App />", () => {
+  mockAxios.get.mockResolvedValue({
+    data: {
+      users
+    }
+  });
+
+  it("should mount the component", async () => {
+    const context = { dispatch: () => {} };
+    const res = await fetchUsers({ context });
+
+    const wrapper = shallow(
+      <MemoryRouter>
+        <App>
+          <Route path="/">
+            <Users />
+          </Route>
+        </App>
+      </MemoryRouter>
+    );
+  });
+});
